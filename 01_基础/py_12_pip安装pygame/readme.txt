991698bea60ddf1c2a5a585449f0d3313eb2c44e
vaunis <vaunis@126.com>
2020-05-19 00:59:43 +0800

    添加to_ts.bat

--- 01_基础/py_12_pip安装pygame/readme.txt
+++ 01_基础/py_12_pip安装pygame/readme.txt
@@ -2,8 +2,16 @@
 1.安装pip3
     sudo apt-get install python3-pip
 2.安装pygame
-    sudo pip3 install pygame (会报错,这个时候可以使用 sudo -H pip3 install pygame；安装目录：/usr/local/lib/python3.6/dist-packages/pygame/)
-    或者 pip3 install pygame ,安装在当前用户目录下(目录：~/.local/lib/python3.6/site-packages/)
+    sudo pip3 install pygame (会报错,这个时候可以使用 sudo -H pip3 install pygame )
+    或者 pip3 install pygame ,安装在当前用户目录下
 
 3.卸载pygame
     sudo pip3 uninstall pygame
+
+
+pip install
+    调用python中的pip库,可使用pip show pip查看调用哪个库
+安装位置:
+    一般为($(python安装目录)\lib\site-packages
+更改pip安装库的位置: 修改python安装目录下的site.py
+
--- 01_基础/py_13_io流/06_生成bat.py
+++ 01_基础/py_13_io流/06_生成bat.py
@@ -1,5 +1,46 @@
 import os
 
+
+# 生成bat脚本文件
+def gene_bat(file_list):
+    print("gene_bat called")
+    final_str = ""
+    initial_str = file_list[0][0:-1]
+
+    i = 0
+    while i < len(file_list):
+        final_str += initial_str + str(i) + "+"
+        i += 1
+    final_str = final_str[0:-1]
+
+    print("final_str:%s" % final_str)
+    bat_cmd = "copy /b " + final_str + " new.ts"
+    print("bat_cmd:%s" % bat_cmd)
+
+    file = open("to_ts.bat", "w")
+    file.write(bat_cmd)
+    file.close()
+
+
+# 直接合并文件
+def gene_video_ts(file_list):
+    initial_str = file_list[0][0:-1]
+    i = 0
+    while i < len(file_list):
+        file_list[i] = initial_str + str(i)
+        i += 1
+    file_video = open("new.ts", "wb");  # 以二进制格式写文件
+    for file_name in file_list:
+        try:
+            temp_file = open(file_name, "rb")  # 以二进制格式读文件
+            content = temp_file.read()
+            file_video.write(content)
+            temp_file.close()
+        except FileNotFoundError as error:
+            print("file not found:%s" % error)
+    file_video.close()
+
+
 file_list = os.listdir(".")
 save_list = []  # 保留当前目录下有用的文件
 for file in file_list:
@@ -12,20 +53,5 @@ print("save_list:%s" % save_list)
 save_list.sort()
 print("save_list:%s" % save_list)
 
-final_str = ""
-initial_str = save_list[0][0:-1]
-
-i = 0
-while i < len(save_list):
-    final_str += initial_str + str(i) + "+"
-    i += 1
-final_str=final_str[0:-1]
-
-
-print("final_str:%s" % final_str)
-bat_cmd = "copy /b "+final_str+" new.ts"
-print("bat_cmd:%s"%bat_cmd)
-
-file = open("to_ts.bat", "w")
-file.write(bat_cmd)
-file.close()
+# gene_bat(save_list)
+gene_video_ts(save_list)
