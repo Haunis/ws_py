a2169af08b2d29da372fa7e6332e73a45b999da5
vaunis <vaunis@126.com>
2020-06-03 08:38:07 +0800

    add tcp download file

--- 01_基础/py_16_tcp_socket/01_tcp_socket_server.py
+++ 01_基础/py_16_tcp_socket/01_tcp_socket_server.py
@@ -4,34 +4,36 @@
 """
 import socket
 
+
 def main():
-	#1.创建监听套接字
-	#监听套接字负责等待新的客户端进行链接（电话局总机）
-	tcp_server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    # 1.创建监听套接字
+    # 监听套接字负责等待新的客户端进行链接（电话局总机）
+    tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	#2.绑定端口
-	local_address = ("",8888) #ip不写，表示本机任意一个ip
-	tcp_server_socket.bind(local_address) 
+    # 2.绑定端口；服务器需要绑定端口
+    local_address = ("", 8888)  # ip不写，表示本机任意一个ip
+    tcp_server_socket.bind(local_address)
 
-	#3.设置监听，让监听套接字由主动变为被动	
-	tcp_server_socket.listen(128)
-	
-	#4.等待客户端链接;accept产生的新的套接字负责为客户端服务(收发数据,理解为具体的人工服务)
-	print("---------begain accept-----------")
-	new_tcp_client_socket,client_address = tcp_server_socket.accept() #阻塞;client_address是元组
-	print("client_address:%s"%str(client_address))
-	print("---------end accept-----------")
-	
-	#5.收发数据
-	receive_data = new_tcp_client_socket.recv(1024) #阻塞；设置接收的最大字节，receive_data是bytes类型
-	print(type(receive_data))
-	print("receive_data:%s"%receive_data.decode("utf-8"))
-	new_tcp_client_socket.send("数据已收到".encode("utf-8"))
+    # 3.设置监听，让监听套接字由主动变为被动;如果不做，只能链接别人，不能让别人链接你;
+    # 参数128可简单理解为并发数，但是分操作系统，写成256，写成10...没啥区别，这个值的大小几乎不会影响同一时刻客户端的多少，最终由操作系统决定
+    tcp_server_socket.listen(128)
 
-	#6.关闭套接字
-	new_tcp_client_socket.close() #不会在为此次链接的客户端服务
-	tcp_server_socket.close() #关闭监听套接字，不会再监听新的客户端的到来,即accept()会失败
-	
+    # 4.等待客户端链接;accept产生的新的套接字负责为客户端服务(收发数据,理解为具体的人工服务),这个套接字标记客户端
+    print("---------begain accept-----------")
+    new_tcp_client_socket, client_address = tcp_server_socket.accept()  # <<<<<阻塞>>>>>;client_address是元组;客户端调用connect(）会解阻塞
+    print("client_address:%s" % str(client_address))
+    print("---------end accept-----------")
+
+    # 5.收发数据
+    receive_data = new_tcp_client_socket.recv(1024)  # <<<<<阻塞>>>>>；设置接收的最大字节，receive_data是bytes类型;解阻塞：1.收到消息 2.客户端关了
+    print(type(receive_data))
+    print("receive_data:%s" % receive_data.decode("utf-8"))
+    new_tcp_client_socket.send("数据已收到".encode("utf-8"))
+
+    # 6.关闭套接字
+    new_tcp_client_socket.close()  # 不会在为此次链接的客户端服务
+    tcp_server_socket.close()  # 关闭监听套接字，不会再监听新的客户端的到来,即accept()会失败
+
 
 if __name__ == "__main__":
-	main()
+    main()
--- 01_基础/py_16_tcp_socket/tcp_download_server.py
+++ 01_基础/py_16_tcp_socket/tcp_download_server.py
@@ -1,39 +1,40 @@
 #! /usr/bin/python3
 import socket
 
+
 def main():
-	#1.创建套接字
-	tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    # 1.创建套接字
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	#2.绑定端口
-	tcp_socket.bind(("",8888))
+    # 2.绑定端口
+    tcp_socket.bind(("", 8888))
 
-	#3.设置监听，让监听套接字由主动变为被动
-	tcp_socket.listen(128)
+    # 3.设置监听，让监听套接字由主动变为被动
+    tcp_socket.listen(128)
 
-	while True:
-		#4.等待客户端连接；客户端连接成功会产生一个新的套接字为客户端服务
-		new_tcp_client_socket ,client_address= tcp_socket.accept() #阻塞
+    while True:
+        # 4.等待客户端连接；客户端连接成功会产生一个新的套接字为客户端服务
+        new_tcp_client_socket, client_address = tcp_socket.accept()  # 阻塞
 
-		#5.接收客户端请求下载的文件名
-		file_name = new_tcp_client_socket.recv(1024).decode("utf-8") #一次接收的最大字节数
-		print("file_name: %s"%file_name)	
+        # 5.接收客户端请求下载的文件名
+        file_name = new_tcp_client_socket.recv(1024).decode("utf-8")  # 一次接收的最大字节数
+        print("file_name: %s" % file_name)
 
-		#6.打开文件并发送文件内容
-		file_content = None
-		try:
-			f = open(file_name,"rb")
-			file_content = f.read()
-			f.close()
-		except Exception as e:
-			print("无下载文件:%s"%str(e))
-		if file_content:
-			new_tcp_client_socket.send(file_content)	
+        # 6.打开文件并发送文件内容
+        file_content = None
+        try:
+            f = open(file_name, "rb")
+            file_content = f.read()
+            f.close()
+        except Exception as e:
+            print("无下载文件:%s" % str(e))
+        if file_content:
+            new_tcp_client_socket.send(file_content)
 
-		#关闭套接字
-		new_tcp_client_socket.close()
-	tcp_socket.close()
-	
+        # 关闭套接字
+        new_tcp_client_socket.close()
+    tcp_socket.close()
+
 
 if __name__ == "__main__":
-	main()
+    main()
