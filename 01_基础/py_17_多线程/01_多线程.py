c891b87f251a7758f2875d0d2fde180a23b8684a
vaunis <vaunis@126.com>
2020-06-08 01:51:36 +0800

    update .gitignore

--- 01_基础/py_17_多线程/01_多线程.py
+++ 01_基础/py_17_多线程/01_多线程.py
@@ -4,8 +4,8 @@
 并发:假的的多任务,单核cpu使用时间片轮转或优先级调度的方式执行(cpu核心数小于任务数)
 电脑里基本都是并发
 
-主线程结束后，进程并不会结束；主线程会等待子线程结束后才结束；
-这跟c++不一样，默认情况下主线程结束了，进程也就结束了
+主线程会等待子线程结束
+这跟c++不一样，默认情况下主线程不会等待子线程结束
 
 """
 import time
@@ -27,9 +27,9 @@ def dance():
 
 
 def main():
-    t1 = threading.Thread(target=sing)
+    t1 = threading.Thread(target=sing)  # 只是创建一个普通的类
     t2 = threading.Thread(target=dance)
-    t1.start()
+    t1.start()  # 创建并运行线程
     t2.start()
     while True:
         global g_loop_count  # 告诉解释器此变量是全局变量不是局部变量;修改此值,会直接修改全局变量的值;否则此变量只是定义一个局部变量
