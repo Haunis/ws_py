684af98f669973ebc51b5f7d50973aa58ef6ec0f
haunis <zj8073@126.com>
2020-07-04 02:14:28 +0800

    update re

--- 01_基础/py_20_正则/01_正则.py
+++ 01_基础/py_20_正则/01_正则.py
@@ -19,13 +19,15 @@
 \W  --匹配非单词字符
 \s  --匹配空格,table键(\t),用的少
 \S  --匹配非空字符
-.   --任意一个字符
+.   --任意一个字符，除了\n;如果要想匹配到"\n"要加re.S参数，即 re.match(r".",str,re.S)
 
 {}  --花括号前面的可以重复几次,如
         \d{1,2}就是1个数字或者两个数字
         \d{1,3}从1到3
         \d{11}只能11位
 ?   --"?"前面的有或者没有,如 "abc-?"匹配abc或者abc-均可
+*   --0或者多个; 如r".*"是匹配任意字符，除了\n（换行）
++   --至少有一个
 
 
 
@@ -58,7 +60,7 @@ print("------------------4.匹配\s-------------------------")
 ret = re.match(r"abc\t", "abc\tdddd")  # ok
 print(ret)
 
-print("------------------5.匹配.-------------------------")
+print("------------------5.匹配点.-------------------------")
 ret = re.match(r"abc.", "abc*dsd")
 print(ret)
 
@@ -69,3 +71,26 @@ print(ret)
 print("------------------7.?-------------------------")
 ret = re.match(r"abc-?\d{3,4}", "abc123")  # "-"有没有均可,数字必须3位或者4位
 print(ret)
+
+print("------------------8.*-------------------------")
+temp_str = """abcde
+addfasiui
+jkjl
+"""
+ret = re.match(r".*", temp_str, re.S)  # 匹配所有字符，包括\n; 没有re.S参数的话，不能匹配\n
+print(ret)
+
+print("------------------9.判断输入的变量名是否有效-------------------------")
+# 有效的变量名：非数字开头的数字字母下划线
+while True:
+    temp_var = input("pleae input 变量名(end结束):")
+    if temp_var == "end":
+        break
+    ret = re.match(r"[a-zA-Z_]+[a-zA-Z0-9_]*", temp_var)  # 字母或下划线开头，后面跟数字字母下划线
+    if ret is None:
+        print("%s 无效" % temp_var)
+    else:
+        if len(ret.group()) == len(temp_var):
+            print("%s 有效" % temp_var)
+        else:  # 如 abc_*#
+            print("%s 无效,含有非法字符：%s" % (temp_var, temp_var[len(ret.group()):]))
