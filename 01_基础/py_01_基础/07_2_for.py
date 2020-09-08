6c81251da88fbf700ddeced523708bcf89c1831c
haunis <zj8073@126.com>
2020-09-08 23:23:34 +0800

    add 算法

--- 01_基础/py_01_基础/07_2_for.py
+++ 01_基础/py_01_基础/07_2_for.py
@@ -10,3 +10,15 @@ for item in list_name:
 else:
     # for循环遍历完成,且未使用break方可执行else里的内容
     print("execute else")
+
+print("------------------3.range--------------------")
+for i in range(10):
+    print(i)
+
+print("------------------4.range--------------------")
+for i in range(5, 10):  # 5,6,7,8,9
+    print(i)
+
+print("------------------5.range--------------------")
+for i in range(5, 10, 2):  # 5,7,9
+    print(i)
